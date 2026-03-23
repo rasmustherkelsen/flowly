d43eb1f6f3135ddf11c3528f9e9442adc7fce038
Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>
2026-03-23 13:59:53 +0100

    Updated sample (#17)
    Reviewed-on: https://gitea.triumfnas.synology.me/rasmustherkelsen/SimpleTransit/pulls/17
    Co-authored-by: Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>
    Co-committed-by: Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>

--- Samples/AzureServiceBus/Aspire/BackendProcessor/JobHandlers/SomeQueryProcessor.cs
+++ Samples/AzureServiceBus/Aspire/BackendProcessor/JobHandlers/SomeQueryProcessor.cs
@@ -9,6 +9,10 @@ public class SomeQueryProcessor : MessageHandlerBase<SomeQueryMessage>
     public override async Task Handle(IMessageContext<SomeQueryMessage> messageContext)
     {
         Console.WriteLine($"SomeQueryProcessor. Waiting for {messageContext.Message.DelayInSeconds} seconds...");
+
+        if (Random.Shared.Next(0, 10) < 5)
+            throw new InvalidOperationException("Kaboom");
+        
         await Task.Delay(TimeSpan.FromSeconds(messageContext.Message.DelayInSeconds));
     }
 }
