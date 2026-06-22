3b1036cdd075e96ce01e56360b99387a364f2d3e
Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>
2026-06-22 16:05:20 +0200

    Cleanup samples (#116)
    Reviewed-on: https://gitea.triumfnas.synology.me/rasmustherkelsen/SimpleTransit/pulls/116
    Co-authored-by: Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>
    Co-committed-by: Rasmus Therkelsen <rasmus_therkelsen@hotmail.com>

--- samples/InMemory/FullSample/Api/Program.cs
+++ samples/InMemory/FullSample/Api/Program.cs
@@ -1,12 +1,8 @@
-using FastEndpoints;
-using FastEndpoints.Swagger;
 using Flowly;
 using Flowly.Dashboard;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
 builder.Services.AddFlowlyDashboard();
 builder.AddFlowly<ApiFlowlyConfiguration>();
 
@@ -14,9 +10,5 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseFlowlyDashboard();
-app.UseFastEndpoints();
-
-if (app.Environment.IsDevelopment())
-    app.UseSwaggerGen();
 
 app.Run();
